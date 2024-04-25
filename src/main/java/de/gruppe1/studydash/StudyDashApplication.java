a72b8db561ac584040bbd453ea595858520ff9a3
Technik122 <66574927+Technik122@users.noreply.github.com>
2024-04-25 10:43:18 +0200

    Initialer Start

---
+++ src/main/java/de/gruppe1/studydash/StudyDashApplication.java
@@ -0,0 +1,13 @@
+package de.gruppe1.studydash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/gruppe1/studydash/StudyDashApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gruppe1.studydash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
